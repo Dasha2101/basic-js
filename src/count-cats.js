35bb127e82b24e9463e8be85c74d7bc2cccb1e5f
Dasha <girl.is.anime20@gmail.com>
2023-12-25 15:27:16 +0300

    refactor: change code

--- src/count-cats.js
+++ src/count-cats.js
@@ -16,10 +16,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function countCats(matrix) {
   let count = 0;
-  for(let i = 0; i < matrix.length; i++){
-    for(let j = 0; j < matrix[i].length; j++){
-      if (matrix[i][j] === '^^'){
-        count++
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      if (matrix[i][j] === '^^') {
+        count += 1
       }
     }
   }
